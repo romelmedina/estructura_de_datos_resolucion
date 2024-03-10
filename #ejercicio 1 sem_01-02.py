83a3299c6a370eab68a7e93f4213ff87eebe7d64
romelmedina <143619114+romelmedina@users.noreply.github.com>
2024-03-09 19:45:37 -0500

    Add files via upload

---
+++ #ejercicio 1 sem_01-02.py
@@ -0,0 +1,34 @@
+#ejercicio 1 sem_01-02
+
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+
+if numero2 != 0:
+    division = numero1 / numero2
+else:
+    division = "No se puede dividir entre cero."
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
+
+#ejercicio 1 sem_01-02
+#ejercicio 2 sem_01-02
+#ejercicio 3 sem_01-02
+#ejercicio 4 sem_01-02
+#ejercicio 5 sem_01-02
+#ejercicio 6 sem_01-02
+#ejercicio 7 sem_01-02
+#ejercicio 8 sem_01-02
+#ejercicio 9 sem_01-02
+#ejercicio 10 sem_01-02
+#ejercicio 11 sem_01-02
+#ejercicio 12 sem_01-02
+#ejercicio 13 sem_01-02
+#ejercicio 14 sem_01-02
+#ejercicio 15 sem_01-02
